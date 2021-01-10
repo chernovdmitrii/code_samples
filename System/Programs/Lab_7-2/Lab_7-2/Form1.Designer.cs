a5535f9acf8fd0f6eaabea1c64063f7c09cc1ce4
chernovdmitrii <55793294+chernovdmitrii@users.noreply.github.com>
2021-01-10 23:46:18 +0300

    Add files via upload

---
+++ System/Programs/Lab_7-2/Lab_7-2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Lab_7_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Location = new System.Drawing.Point(42, 367);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(161, 56);
+            this.buttonPrev.TabIndex = 0;
+            this.buttonPrev.Text = "Previous";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(516, 367);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(161, 56);
+            this.buttonNext.TabIndex = 1;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(281, 367);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(161, 56);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Cleare Screen";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 456);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrev);
+            this.Name = "Form1";
+            this.Text = "La France";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ System/Programs/Lab_7-2/Lab_7-2/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Drawing2D;
+
+namespace Lab_7_2
+{
+    public partial class Form1 : Form
+    {
+        //Global variables
+        Bitmap pic_paris;
+        Bitmap pic_paris_rue;
+        Bitmap pic_lille;
+        Bitmap pic_biarritz; 
+        string france_path;
+        int count = 0;
+        Font btmFont;
+        
+        public Form1()
+        {
+            france_path = Application.StartupPath + "\\France";
+            pic_paris = new Bitmap(france_path + "\\paris.bmp");
+            pic_paris_rue = new Bitmap(france_path + "\\paris_rue.bmp");
+            pic_lille = new Bitmap(france_path + "\\lille.bmp");
+            pic_biarritz = new Bitmap(france_path + "\\biarritz.bmp");
+
+            btmFont = new Font("Courier New", 14, FontStyle.Bold, GraphicsUnit.Pixel);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        { 
+
+
+            if (count < 5)
+            {
+                count -= 1;
+            }
+            if (count == 5)
+            {
+                count = 1;
+            }
+            if (count <= 0)
+            {
+                count = 4;
+            }
+
+            drawPersentation(count);
+           //Invalidate();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (count <= 5)
+            {
+                count += 1;
+            }
+            if (count == 5)
+            {
+                count = 1;
+            }
+            drawPersentation(count);
+            //Invalidate();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void drawPersentation(int _count)
+        {
+            string text = ""; 
+            switch (_count)
+            {
+                case 0:
+                    Console.WriteLine(count);
+                    break;
+                case 1:
+                    text = "Vue de Paris de la fenêtre";
+                    drawSlide(pic_paris, text);
+                    Console.WriteLine(count);
+                    break;
+                case 2:
+                    text = "Passage piéton parisien avec le feu rouge"; 
+                    drawSlide(pic_paris_rue, text);
+                    Console.WriteLine(count);
+                    break;
+                case 3:
+                    text = "Entrée du night club"; 
+                    drawSlide(pic_lille, text);
+                    Console.WriteLine(count);
+                    break;
+                case 4:
+                    text = "La phare de Biarritz"; 
+                    drawSlide(pic_biarritz, text);
+                    Console.WriteLine(count);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void drawSlide(Bitmap slide, string text)
+        {
+            Graphics dc = this.CreateGraphics();
+           
+
+            for (int i = 100; i > 1; i--)
+            {
+                //this.Refresh();
+                Thread.Sleep(50);
+                dc.FillRectangle(Brushes.White, 0, 0, 700, 700);
+                dc.DrawImage(slide, 20 + i * 4, 10, slide.Width / 3, slide.Height / 3);
+                Point pos = new Point(20 + i * 4, 10 + slide.Height / 3); 
+                dc.DrawString(text, btmFont, Brushes.Black, pos) ;
+            }
+        }
+    }
+}
